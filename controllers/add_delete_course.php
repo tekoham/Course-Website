2f26dcfc2dc9fdc9bf5024dc0ac2fc31e61f9a13
DaKi <62581579+tekoham@users.noreply.github.com>
2020-05-04 20:31:54 +0700

    Add files via upload

---
+++ controllers/add_delete_course.php
@@ -0,0 +1,19 @@
+<?php
+    $connect = mysqli_connect('localhost','DaoKien','29082000','website_database');
+    $course_name = $type = $description = $image_link = $youtube_link = $teacher_id = "";
+
+    if(isset($_POST['add'])){
+        $course_name = $_POST['course_name'];
+        $type = $_POST['type'];
+        $description = $_POST['description'];
+        $image_link = $_POST['image_link'];
+        $youtube_link = $_POST['youtube_link'];
+        $teacher_id = $_POST['teacher_id'];
+        $result = mysqli_query($connect, "INSERT INTO courses (course_name, type, description, image_link, youtube_link, teacher_id) VALUE ('$course_name', '$type', '$description', '$image_link', '$youtube_link', '$teacher_id');");
+    }
+
+    if(isset($_POST['Delete'])){
+        $course_id = $_POST['Delete'];
+        $result = mysqli_query($connect, "DELETE FROM courses WHERE course_id = $course_id ;");
+    }
+?>
